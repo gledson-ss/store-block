9062ff3dcef3f384f288291eddffc12cc8d5e580
GledsonS831 <glegogle84@gmail.com>
2020-08-17 17:29:45 -0300

    :hammer: update project

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,26 +2,51 @@ import React, { useState } from "react";
 import { TimeSplit } from "./typings/global";
 import { tick } from "./utils/time";
 
-interface CountdownProps {
-  targetDate: string;
-}
+import { useCssHandles } from 'vtex.css-handles'
+import { useQuery } from "react-apollo";
 
-const DEFAULT_TARGET_DATE = new Date("2020-06-25").toISOString();
+import useProduct from "vtex.product-context/useProduct";
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE,
-}) => {
+import productReleaseDate from "./queries/productReleaseDate.graphql";
+
+const DEFAULT_TARGET_DATE = new Date("2020-03-02").toISOString();
+const CSS_HANDLES = ['countdown']
+const Countdown: StorefrontFunctionComponent = () => {
+
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: "00",
     minutes: "00",
     seconds: "00",
   });
-  tick(targetDate, setTime);
+  const handles = useCssHandles(CSS_HANDLES)
+  const productContext = useProduct()
+  const product = productContext?.product
+  const linkText = product.linkText
+  // @ts-ignore
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText,
+    },
+    ssr: false,
+  });
+  console.log({data})
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  if (loading) {
+    return <span>Loading...</span>
+  }
+
+  if (error) {
+    return <span>Erro!</span>
+  }
+
+
   return (
-    <div>
-      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
-  );
+  ) 
 };
 
 Countdown.schema = {
@@ -29,12 +54,7 @@ Countdown.schema = {
   description: "editor.countdown.description",
   type: "object",
   properties: {
-    targetDate: {
-      title: "Final date",
-      description: "Final date used in the countdown",
-      type: "string",
-      default: null,
-    },
+    
   },
 };
 
